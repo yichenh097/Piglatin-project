787cfb44ff77f50c334e109ab59fb2d1fb9140a9
yichenh097 <1.yichen.hu@gmail.com>
2024-11-20 03:25:20 +0000

    Pending changes exported from your codespace

--- src/App.java
+++ src/App.java
@@ -1,7 +1,30 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 class App {
   public static void main(String[] args) {
-    Book aBook = new Book();
-    System.out.println(aBook.pigLatin("hello"));
-    System.out.println(aBook.translateWord(""));
+    Book Book = new Book("https://www.gutenberg.org/cache/epub/11/pg11.txt");
+    try{
+      File myFile = new File("book.txt");
+      if (myFile.createNewFile()){
+        System.out.println("created");
+      } else {
+        System.out.println("no");
+      }
+    } catch (IOException e){
+      System.out.println("error");
+      e.printStackTrace();
+    }
+    try {
+      FileWriter write = new FileWriter("book.txt");
+      write.write(Book.pigLatin(Book.toString()));
+      write.close();
+    } catch(IOException e){
+      e.printStackTrace();
+    }
+    //System.out.println(aBook.pigLatin("I can now spreak in a Pig-Latin. I can translate words and sentences for the students at Foothill High School."));
   }
+    
+    //System.out.println(Book.pigLatin("Translation Test I. I! I \"Bam this should work\". Last! word"));
 }
+
--- src/Book.java
+++ src/Book.java
@@ -3,32 +3,101 @@
 //Words will be separated by spaces. 
 //There can be punctuation in a word, we will only add/keep punctuation at the end of a string if it is at the end of a string.
 //    for examples: Hello.==> Ellohay.    Good-bye! ==> Ood-byegay!    so... ==> osay...
-
+import java.io .IOException;
+import java.util.Scanner;
+import java.net.URL;
 public class Book
 {
-  public String pigLatin(String word)
-  {
+  private static final String punctuation = ".!?,\':;\"";
 
+  public static String pigLatin(String word)
+  {
+     String currentWord = "";
+     int p = 0;
+     boolean active = false;
+     int wordCount = 0;
+
+      for (int i = 0; i < word.length(); i++) {
+        if ( i < 30000){
+          if(word.substring(i, i + 3).equals("***")){
+            currentWord = "";
+            active = true;
+            System.out.println(word.substring(i, i + 3));
+          }
+        }
+
+          if ( i > 100000){
+            if(word.substring(i, i + 3).equals("***")){
+              System.out.println(word.substring(i,i+3));
+              System.out.println(wordCount + " (this is excluding : top and bottom legal stuff (ALICE))");
+              return currentWord;
+            }
+          }
+        
+        if(active){
+          if (word.charAt(i) == ' ' && word.charAt(i + 1) != ' '){
+            String first = word.substring(p, p + 1);
+            String last = word.substring(p + 1, i);
+            String vowels = "aeiou";
+            if(p == i - 1){
+              currentWord += " " + word.charAt(i-1) + "yay"; // if single letter
+            } else if (vowels.indexOf(first) >= 0){
+              currentWord += word.substring(p,i) + "yay";
+            }
+            else if ((punctuation.indexOf(Character.toString(word.charAt(i-1))) > -1) || (punctuation.indexOf(Character.toString(word.charAt(p))) > -1)){
+              if(punctuation.indexOf(Character.toString(word.charAt(p))) > -1){ //if caps in front
+                currentWord += " " + word.charAt(p) + Character.toUpperCase(word.charAt(p+2)) + word.substring(p + 3, i ) + Character.toLowerCase(word.charAt( p+1 )) +  "ay";
+              } else if(word.charAt(i - 3) == ' '){ //does not count for "oneword"
+                currentWord += " " + word.charAt(i-2) + "yay" + word.charAt(i-1); //if single letter and punc
+              } else { //just punc
+                currentWord += " " + Character.toUpperCase(last.charAt(0)) + last.substring(1, last.length() - 1) + Character.toLowerCase(first.charAt(0)) + "ay" + word.charAt(i-1);
+              }
+            } 
+            else if (Character.isUpperCase(first.charAt(0))) { //notcaps
+              currentWord += " " + Character.toUpperCase(last.charAt(0)) + last.substring(1) + Character.toLowerCase(first.charAt(0)) + "ay"; 
+            } else {
+              currentWord += " " + word.substring(p + 1, i) + first + "ay";
+            }
+            wordCount ++; 
+            p = i + 1;
+          } else {
+            continue;
+          }
+        }
+        
+    }
+    String first = word.substring(p, p + 1);
+    String last = word.substring(p + 1);
+    if (Character.isUpperCase(first.charAt(0))) {
+      currentWord += " " + Character.toUpperCase(last.charAt(0)) + last.substring(1, last.length() - 1) + Character.toLowerCase(first.charAt(0)) + "ay" + "."; 
+    } else {
+      currentWord += " " + word.substring(p + 1) + first + "ay";
+    }
+    System.out.println(wordCount);
+    return currentWord;
   }
   
-  public int endPunctuation(String word)  //return the index of where the punctuation is at the end of a String. If it is all punctuation return 0, if there is no punctuation return -1
-  {
-
-    return -1;
+  
+  private String bookText;
+  
+  public Book(String link){
+      readBook(link);
   }
-
-  public String translateWord(String word)    //to share with class
-  {
-    String convertedWord = "";
-
-    return convertedWord;
+  private void readBook(String link){
+   try {
+    URL url = new URL(link);
+    Scanner s = new Scanner(url.openStream());
+    while(s.hasNext()){
+      String text = s.nextLine();
+      bookText += text;
+    }
+   }
+   catch(IOException ex) {
+      ex.printStackTrace();
+   }
   }
-
-  public String translateSentence(String sentence)
-  {
-    String retSentence = "";
-
-
-    return retSentence;
+  @Override
+  public String toString(){
+   return bookText;
   }
 }  
